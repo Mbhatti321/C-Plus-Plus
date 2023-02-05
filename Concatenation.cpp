a87c80c9f5f47ce6517d588df8f59305bb76af6b
Muneeb Bhatti <124075068+Mbhatti321@users.noreply.github.com>
2023-02-05 23:12:14 +0500

    Add files via upload

---
+++ Concatenation.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string fullname;
+	string f_name="Muneeb ";
+	string l_name="Bhatti";
+	fullname=f_name+l_name;
+	cout<<fullname;
+	return 0;
+	}
