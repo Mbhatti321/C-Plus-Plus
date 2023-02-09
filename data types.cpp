b33bdcb3ae5dcaa86e62dc088f9bdce18826ea80
Muneeb Bhatti <124075068+Mbhatti321@users.noreply.github.com>
2023-02-09 10:04:06 +0500

    Add files via upload

---
+++ data types.cpp
@@ -0,0 +1,16 @@
+#include<iostream> // THIS IS LIBRARY
+using namespace std; // THIS IS STANDARD
+int main()  // THIS IS MAIN FUNCTION
+{ // open brace
+	int num=10; // int data type initialization
+	float dig=10.98; // float data type initilization
+	double hel=1873.2987;// double data type initilization
+	char marks='c';// character data type initilization
+	string start="Hello";// string data type initilization
+	cout<<num<<"\n"; // calling num value
+	cout<<dig<<"\n";// calling dig value
+	cout<<hel<<"\n";// calling hel value
+	cout<<marks<<"\n";// calling marks value
+	cout<<start<<"\n";// calling start value
+	return 0;
+} // closing brace
